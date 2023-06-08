332a1b041e3ef99972532fa14db41bb9d6c5cc7b
hunnikim <youngsk0332@naver.com>
2023-06-08 21:13:07 +0900

    test

--- src/App.js
+++ src/App.js
@@ -17,10 +17,7 @@ const App= ()=> {
   const Writing = loadable(()=> import('./pages/Community/Writing'));
   const WritingList = loadable(()=> import('./pages/Community/writingList'));
   const GetWriting = loadable(()=> import('./pages/Community/getWriting'));
-  const Chat = loadable(()=> import('./pages/Chat'));
-  const Ticket = loadable(()=> import('./pages/Ticket'));
-  const Exchange = loadable(()=> import('./pages/Exchange'))
-  const Admin = loadable(()=> import('./pages/Admin'))
+  const Chat = loadable(()=> import('./pages/Chat'))
   const StreamerChat = loadable(()=> import('./pages/StreamerChat'));
   const Test = loadable(()=> import('./pages/SearchPage'));
   const LoadingPage = loadable(()=> import('./pages/LoadingPage'));
@@ -37,6 +34,21 @@ const App= ()=> {
         <Route path="/WritingList/:userId" element={<WritingList/>}/>
         <Route path="/:writingNo/:userId" element={<GetWriting/>}/>
         <Route path="/Chat" element={<Chat/>} />
+        <Route path="/StreamerChat" element={<StreamerChat/>} />
+        <Route path="/Test" element={<Test/>} />
+        {/* <Route path="/addUser" element={<AddUser/>} /> */}
+        {/* <Route path="/sendSMS" element={<SendSMS/>} /> */}
+        {/* <Route path='/remove' element={<RmUser/>} /> */}
+        {/* <Route path='/updateUser' element={<UpdateUser/>} /> */}
+        <Route path="/LoadingPage" element={<LoadingPage/>} />
+        <Route exact path="/admin/ad" element={<Ad/>} />
+        <Route exact path="/admin/adReq" element={<AdReq/>} />
+        <Route exact path="/admin/report" element={<Report/>} />
+        <Route exact path="/admin/adminStreamingList" element={<AdminStreamingList/>} />
+        <Route exact path="/admin/adminUserList" element={<AdminUserList/>} />
+        <Route exact path="/admin/streamingBanList" element={<StreamingBanList/>} />
+        <Route exact path="/admin/streamingRollBanList" element={<StreamingRollBanList/>} />
+        <Route path="/SearchKeyword/:search" element={<SearchKeyword/>} />
       </Routes>
     </Router>
     
